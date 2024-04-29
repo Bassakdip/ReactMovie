52c67c43fcbe3c2823580488be4aad9921ab4867
Dipankar Basak <143477069+Bassakdip@users.noreply.github.com>
2024-04-29 12:53:33 +0530

    Error App.js

--- src/App.js
+++ src/App.js
@@ -5,21 +5,32 @@ import MoviesList from './components/MoviesList';
 
 function App() {
   const [movies, setMovies] = useState([]);
-  const [isloading, setIsLoading] = useState(false)
+  const [isloading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   async function fetchMoviesHandler() {
     setIsLoading(true);
-    const response = await fetch('https://swapi.py4e.com/api/films/')
-    const data = await response.json();
-    const transformedMovies = data.results.map(movieData => {
-      return {
-        id: movieData.episode_id,
-        title: movieData.title,
-        openingText: movieData.opening_crawl,
-        releaseDate: movieData.release_date
+    setError(null);
+    try {
+      const response = await fetch('https://swapi.py4e.com/api/films/')
+      if (!response.ok) {
+        throw new Error("Something went wrong...retrying");
       }
-    })
-    setMovies(transformedMovies);
+      const data = await response.json();
+
+      const transformedMovies = data.results.map(movieData => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date
+        }
+      })
+      setMovies(transformedMovies);
+    }
+    catch(error){
+      setError(error.message);
+    }
     setIsLoading(false);
   }
   return (
@@ -28,8 +39,9 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        {!isloading && movies.length > 0 && <MoviesList movies={movies}/>}
-        {!isloading && movies.length === 0 && <p>Found no movies</p>}
+        {!isloading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isloading && movies.length === 0 && !error && <p>Found no movies</p>}
+        {!isloading && error && <p>{error}</p>}
         {isloading && <p>Loading...</p>}
       </section>
     </React.Fragment>
